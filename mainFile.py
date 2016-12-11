258bc777806cace1bf26927fb0738e592afd29e0
niroshana <niroshanakumararathna@gmail.com>
2016-12-11 15:11:45 +0800

    uploaded main file

--- mainFile.py
+++ mainFile.py
@@ -1,3 +1,14 @@
 import openpyxl
-wb = openpyxl.load_workbook('New Microsoft Excel Worksheet.xlsx')
-print(wb.get_sheet_names())
+import os
+
+source_file = 'Autonomation update Central team.xlsx'
+result_file = 'result.xlsx'
+
+if not os.path.isfile(source_file):
+    print('source file dose not exists')
+    exit()
+else:
+    print('source file exists')
+
+wb = openpyxl.load_workbook(source_file)
+print(wb.get_sheet_names().__len__())
