6b08ee517c75d80342b485eae8f003544ff31eb2
niroshana <niroshanakumararathna@gmail.com>
2016-12-14 02:15:26 +0800

    Read BW file

--- mainFile.py
+++ mainFile.py
@@ -2,7 +2,7 @@
 import openpyxl
 import os
 
-source_file = 'Autonomation update Central team.xlsx'
+source_file = 'BW22 Nov.xlsx'
 result_file = 'result.xlsx'
 
 if not os.path.isfile(source_file):
@@ -12,7 +12,14 @@ else:
     print('source file exists')
 
 wb = openpyxl.load_workbook(source_file)
-print(wb.get_sheet_names().__len__())
+ws = wb.get_active_sheet()
+
+print(ws['A'][3].value+'\t\t'+ws['B'][3].value+'\t\t'+ws['F'][2].value+'\t\t'+ws['G'][2].value+'\t\t'+ws['H'][2].value+'\t\t'+ws['I'][2].value+'\t\t'+ws['J'][2].value+'\t\t'+ws['K'][2].value+'\t\t')
+
+
+for i in range(1, ws.max_row - ws.min_row):
+    if ws['B'][i].value == 'Result':
+        print(str(ws['A'][i].value) + '\t\t' + ws['B'][i].value + '\t\t' + str(ws['F'][i].value) + '\t\t' + str(ws['G'][i].value) + '\t\t' + str(ws['H'][i].value) + '\t\t' + str(ws['I'][i].value) + '\t\t' + str(ws['J'][i].value) + '\t\t' + str(ws['K'][i].value) )
 
 if os.path.isfile(result_file):
     os.remove(result_file)
