a08b3ce431bc27e92d7e7a2defdfa7a98dd63961
niroshana <niroshanakumararathna@gmail.com>
2016-12-14 15:01:34 +0800

    File saved successfully

--- mainFile.py
+++ mainFile.py
@@ -29,17 +29,17 @@ result_wb_ws['E'][0].value = ws['H'][2].value
 result_wb_ws['F'][0].value = ws['I'][2].value
 result_wb_ws['G'][0].value = ws['J'][2].value
 result_wb.save(result_file)
-written_row_count = int(1)
+written_row_count = 2
 
 for i in range(1, ws.max_row - ws.min_row):
     if ws['B'][i].value == 'Result':
-        result_wb_ws.cell(row=written_row_count, column=2).value = ws['A'][i].value
-        result_wb_ws.cell(row=written_row_count, column=3).value = ws['B'][i].value
-        result_wb_ws.cell(row=written_row_count, column=4).value = ws['F'][i].value
-        result_wb_ws.cell(row=written_row_count, column=5).value = ws['G'][i].value
-        result_wb_ws.cell(row=written_row_count, column=6).value = ws['H'][i].value
-        result_wb_ws.cell(row=written_row_count, column=7).value = ws['I'][i].value
-        result_wb_ws.cell(row=written_row_count, column=8).value = ws['J'][i].value
+        result_wb_ws.cell(row=written_row_count, column=1).value = ws['A'][i].value
+        result_wb_ws.cell(row=written_row_count, column=2).value = ws['B'][i].value
+        result_wb_ws.cell(row=written_row_count, column=3).value = ws['F'][i].value
+        result_wb_ws.cell(row=written_row_count, column=4).value = ws['G'][i].value
+        result_wb_ws.cell(row=written_row_count, column=5).value = ws['H'][i].value
+        result_wb_ws.cell(row=written_row_count, column=6).value = ws['I'][i].value
+        result_wb_ws.cell(row=written_row_count, column=7).value = ws['J'][i].value
         print('.', sep=' ', end='', flush=True)
         written_row_count += 1
         #print(str(ws['A'][i].value) + '\t\t' + ws['B'][i].value + '\t\t' + str(ws['F'][i].value) + '\t\t' + str(ws['G'][i].value) + '\t\t' + str(ws['H'][i].value) + '\t\t' + str(ws['I'][i].value) + '\t\t' + str(ws['J'][i].value) + '\t\t' + str(ws['K'][i].value) )
